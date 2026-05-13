12341469fa83f32d9eea5d1a59ece9a72530e6e9
Shivansh1211 <shivanshkumar1112@gmail.com>
2026-05-14 02:40:13 +0530

    Initial commit

---
+++ todo-app/src/test/java/com/github/Shivansh1211/todo_app/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Shivansh1211.todo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
